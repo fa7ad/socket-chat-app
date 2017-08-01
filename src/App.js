5add676d045f5fbffd0e17ca65b4c803876b0509
Fahad Hossain <evilinfinite7@gmail.com>
2017-08-01 13:32:54 +0600

    add some css tweaks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { inject, observer } from 'mobx-react'
 
 import './App.css'
 import Modal from './Modal'
 import ChatBox from './ChatBox'
 
-class App extends Component {
-  render () {
-    const { ui } = this.props
-    return (
-      <div className={`App${ui.showName ? ' name' : ''}`}>
-        <Modal />
-        <ChatBox />
-      </div>
-    )
-  }
-}
+const App = props => (
+  <div className={`App${props.ui.showName ? ' name' : ''}`}>
+    <Modal />
+    <ChatBox />
+  </div>
+)
 
 export default inject('ui')(observer(App))
